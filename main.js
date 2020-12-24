2cb0d103995bdda55efa0cf4c6c88ca2a2bdd576
Jacob <jlisic@kent.edu>
2020-12-24 15:45:13 -0500

    Removes sticky nav, fixes footer location issue

--- main.js
+++ main.js
@@ -54,10 +54,10 @@ var header = document.getElementById("nav");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
