f79a22f1d85fe2491cfdf0277e3b40a3e4d0a3a5
Jacob <jlisic@kent.edu>
2021-01-06 22:26:27 -0500

    Accessibility fixes, link size fix

--- main.js
+++ main.js
@@ -45,13 +45,13 @@ darkModeToggle.addEventListener('click', () => {
 
 // Mobile Sticky Nav on Scroll
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("nav");
+// var header = document.getElementById("nav");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+// var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 // function myFunction() {
